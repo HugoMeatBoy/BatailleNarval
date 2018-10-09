d07c612d973143011e3f8c428104f638256f3835
Unknown <hugo.fazio@aol.com>
2018-10-09 23:42:03 +0200

    IA comments

--- src/main/scala/players/IA.scala
+++ src/main/scala/players/IA.scala
@@ -79,7 +79,7 @@ class IAlvl2 extends Player{
 
     var searchNewBoat:Boolean=true
 
-
+    //If a cell has been found randomly and the boat is not sunk yet, try to turn around the touched cell
     def shoot(g: Grid):Tuple2[Int,Int]={
         if(firstCellTouched != null){
             if(!searchNewBoat){
@@ -93,32 +93,43 @@ class IAlvl2 extends Player{
     }
 
 
-
+    //Trying to target last boat touched
     def shootLastBoat(g: Grid, index: Int = 1): Tuple2[Int,Int]={
         var x = firstCellTouched._1
         var y = firstCellTouched._2
 
 
+        //Check in any direction (even diagonals) if there is something else than an old shot or the limit of the grid, and shoot
+
+        //Left
         if(checkNextCell(x+index,y,g)){
             (x+index,y)
         }else if(checkNextCell(x+index,y+index,g)){
             (x+index,y+index)
-        }else if(checkNextCell(x,y+index,g)){
+        }
+        //Down
+        else if(checkNextCell(x,y+index,g)){
             (x, y+index)
         }else if(checkNextCell(x-index,y+index,g)){
             (x-index,y+index)
-        }else if(checkNextCell(x-index, y,g)){
-            (x-index, y)
         }
-        else if(checkNextCell(x-index,y-index,g)){
+        //Right
+        else if(checkNextCell(x-index, y,g)){
+            (x-index, y)
+        }else if(checkNextCell(x-index,y-index,g)){
             (x-index,y-index)
         }else if(checkNextCell(x, y-index,g)){
             (x, y-index)
-        }else if(checkNextCell(x+index, y-index,g)){
+        }
+        //Up
+        else if(checkNextCell(x+index, y-index,g)){
             (x+index, y-index)
         }else{
+            //Max size boat reached, so random pick
             if(index == 5){
                 pickRandomCell
+
+            //Try to target a boat, with a bigger range
             }else{
                 shootLastBoat(g, index+1)
             }
@@ -127,7 +138,8 @@ class IAlvl2 extends Player{
     }
 
 
-    def checkNextCell(x: Int, y: Int, g: Grid):Boolean={
+    //Check if the next possible cell to shoot is not already shot, or off grid
+    def checkNextCell(x: Int, y: Int, g: Grid): Boolean={
         if(x < 10 && x >= 0 && y < 10 && y >= 0){
             g.checkCell(x,y) match {
                 case "Missed" | "Touched" | "Sunk" => false
@@ -150,16 +162,15 @@ class IAlvl2 extends Player{
                 var boat = g.getHitBoat(cellShot._1,cellShot._2)
                 boat.hit
 
-                if(boat.aliveCells==0){
-
-                    searchNewBoat = true
+                if(boat.aliveCells==0){                                 //If ship sank
+                    searchNewBoat = true                                //Reset the search of a new boat, and go back to random cell pick
 
                     boat.sunk = true
                     g.boatSunk(boat)
                     ("\n\n ** X : IA sank a boat !!!\n\n ******************")
                 }else{
                     if(searchNewBoat){
-                        searchNewBoat = false
+                        searchNewBoat = false                           //Keep this boat in target and start firing around
                         firstCellTouched = (cellShot._1,cellShot._2)
                     }
 
@@ -185,10 +196,17 @@ class IAlvl2 extends Player{
 
 /*******************************************************************************************************************/
 
+/*
+    LEVEL 3
 
+    * firstCellTouched : First cell touched of the last boat
+    * lastCellTouched : Last cell touched of the last boat
+*/
 
 class IAlvl3 extends Player {
     var grid = new Grid()
+
+
     var firstCellTouched:Tuple2[Int,Int]=null
     var lastCellTouched:Tuple2[Int,Int]=null
 
@@ -196,6 +214,7 @@ class IAlvl3 extends Player {
 
 
     def shoot(g: Grid):Tuple2[Int,Int]={
+        //As the first boat is touched, try to check if a touched Cell is not sunk
         if(firstCellTouched != null){
             shootLastBoat(g)
         }else{
@@ -204,13 +223,14 @@ class IAlvl3 extends Player {
     }
 
 
-
+    //Retrieve last boat touched and target him
     def shootLastBoat(g: Grid, index: Int = 1): Tuple2[Int,Int]={
         var xF = firstCellTouched._1
         var yF = firstCellTouched._2
         var xL = lastCellTouched._1
         var yL = lastCellTouched._2
 
+        //Check if first and last cell are on the same line (axis x) and try to find cells in this axis where it's ossible to shoot
         if(xF == xL && yL != yF && checkNextCell(xL,yL+index,g)){
             (xL,yL+index)
         }else if(xF == xL && yL != yF && checkNextCell(xL,yL-index,g)){
@@ -219,7 +239,10 @@ class IAlvl3 extends Player {
             (xF,yF+index)
         }else if(xF == xL && yL != yF && checkNextCell(xF,yF-index,g)){
             (xF,yF-index)
-        }else if(xF != xL && yL == yF && checkNextCell(xL+index,yL,g)){
+        }
+
+        //Check if first and last cell are on the same line (axis x) and try to find cells in this axis where it's ossible to shoot
+        else if(xF != xL && yL == yF && checkNextCell(xL+index,yL,g)){
             (xL+index,yL)
         }else if(xF != xL && yL == yF && checkNextCell(xL-index,yL,g)){
             (xL-index,yL)
@@ -228,10 +251,15 @@ class IAlvl3 extends Player {
         }else if(xF != xL && yL == yF && checkNextCell(xF-index,yF,g)){
             (xF-index,yF)
 
+        //If the last boat sank, try to find and old cell touched
+        //Option return to prevent errors if confusion, so pick a random cell
         }else if(xF != xL && yF != yL){
             searchOldHits(g).getOrElse(pickRandomCell)
+
+
         }else{
 
+            //Check around the first touched cell as AI level 2
             if(checkNextCell(xF+index,yF,g)){
                 (xF+index,yF)
             }else if(checkNextCell(xF,yF+index,g)){
@@ -262,7 +290,7 @@ class IAlvl3 extends Player {
         }
     }
 
-
+    //Try to find old hits in ships not sunk
     def searchOldHits( g: Grid, x: Int = 0, y: Int = 0): Option[Tuple2[Int,Int]]={
         g.checkCell(x,y) match {
             case "Touched" => Some((x,y))
@@ -293,16 +321,14 @@ class IAlvl3 extends Player {
                 boat.hit
 
                 if(boat.aliveCells==0){
-
-                    searchNewBoat = true                                //last targeted boat destroyed, back to search
-
+                    searchNewBoat = true
                     boat.sunk = true
                     g.boatSunk(boat)
                     ("\n\n ** X : IA sank a boat !!!\n\n ******************")
                 }else{
                     if(searchNewBoat){
                         searchNewBoat = false
-                        firstCellTouched = (cellShot._1,cellShot._2)
+                        firstCellTouched = (cellShot._1,cellShot._2)            //save lastCellTouched
                     }
                     lastCellTouched = (cellShot._1,cellShot._2)
 
